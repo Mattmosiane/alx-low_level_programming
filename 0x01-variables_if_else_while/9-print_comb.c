a20633c21358975e0614b714edbccfc8a6b5df2f
Mattmosiane <matt.mosiane@gmail.com>
2023-06-18 12:48:43 -0700

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a < 9);
+		if (a < 9)
+		{
+			putchar(',');
+			puthcar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
