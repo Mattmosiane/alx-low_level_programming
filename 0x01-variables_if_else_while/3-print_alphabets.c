a9ccac56e3f80bcea381d97546254e62c5def8a5
Mattmosiane <matt.mosiane@gmail.com>
2023-06-18 10:34:11 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,16 +15,14 @@ int main(void)
 	c = 'a';
 	d = 'A';
 	while
-		(c <= 'z')
-		{
-	putchar(c);
-	c++;
+		(c <= 'z') {
+			putchar(c);
+			c++;
 		}
 	while
-		(d <= 'Z')
-		{
-	putchar(d);
-		d++;
+		(d <= 'Z') {
+			putchar(d);
+			d++;
 		}
 	putchar('\n');
 	return (0);
