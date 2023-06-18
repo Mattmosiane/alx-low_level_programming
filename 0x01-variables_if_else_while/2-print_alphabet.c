609357723a1196a8af08086dc004c3d6652c6562
Mattmosiane <matt.mosiane@gmail.com>
2023-06-18 10:06:05 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putcha('\n');
+	return(0);
+}
+
