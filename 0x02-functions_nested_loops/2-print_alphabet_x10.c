3496bcb6c3e37d10be26f04dc7d9d4aed055fb3d
Mattmosiane <matt.mosiane@gmail.com>
2023-06-20 15:02:11 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Make alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+	i = 0;
+	while (i < 0)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
