e93121ac6cfc53c47ce70896c9ac4d1e0a48c29b
Mattmosiane <matt.mosiane@gmail.com>
2023-06-20 15:44:17 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,6 +8,7 @@
 
 void print_alphabet_x10(void)
 {
+
 	char ch;
 	int i;
 	i = 0;
@@ -15,7 +16,7 @@ void print_alphabet_x10(void)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
-			_putchar(ch);
+		_putchar(ch);
 		}
 		_putchar('\n');
 		i++;
