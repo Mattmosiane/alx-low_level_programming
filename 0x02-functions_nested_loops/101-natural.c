e35af946ca860e25f2958e4c87a7c2595b0f16bc
Mattmosiane <matt.mosiane@gmail.com>
2023-06-21 13:44:18 -0700

    Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,18 +6,18 @@
  */
 
 int main(void)
-
 {
 	int i, z = 0;
 
 	while (i < 1024)
 	{
-	if ((i % 3 == 0) || (i % 5 == 0))
-	{
-	z += i;
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
 	}
-	i++;
-	}
-	printf("%d", z);
+	printf("%d\n", z);
 	return (0);
+
 }
