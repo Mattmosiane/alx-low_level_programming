949a2bb27657b84a5d118878cdf7f1cafd2d3da0
Mattmosiane <matt.mosiane@gmail.com>
2023-06-21 14:37:43 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,16 +2,15 @@
 
 /**
  * main - Prints the sum of Even Fibonacci numbers
- * less than 4000000.
+ * less than 4000000
  * Return: Nothing!
  */
 
 int main(void)
-
 {
 	int i = 0;
 	long j = 1, k = 2, sum = k;
-	
+
 	while (k + j < 4000000)
 	{
 	k += j;
