c171e99f7ca16d6ba44e774493540f73a61c1948
Mattmosiane <matt.mosiane@gmail.com>
2023-06-21 13:19:23 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,15 +10,16 @@ int main(void)
 
 {
 
-	int i, z = 0
-		while (i < 1024)
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			if ((i % 3 == 0) || (i % 5 == 0))
-			{
-				z += i;
-			}
-			i++;
+			z += i;
 		}
+		i++;
+	}
 	printf("%d", z);
 	return (0);
 }
