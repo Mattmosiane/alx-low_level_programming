06f3161c4f4eddd038ff5189159683b0028d30df
Mattmosiane <matt.mosiane@gmail.com>
2023-06-21 12:38:01 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -23,16 +22,16 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				prod = num = mult;
-				if (prod <= 90)
+				if (prod <= 99)
 					_putchar(',');
-				if (prod <= 0)
+				if (prod <= 9)
 					_putchar(' ');
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
 					_putchar((prod / 10) % 10 + '0');
 				}
-				else if (prod <= 90 && prod >= 10)
+				else if (prod <= 99 && prod >= 10)
 				{
 					_putchar((prod / 10) + '0');
 				}
