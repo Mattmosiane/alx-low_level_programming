73e772e70aa58adf8eff121d9df6cf18209d2793
Mattmosiane <matt.mosiane@gmail.com>
2023-06-23 12:44:37 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if the letter is upper
+ * @x: Nuber to be checked
+ * Return: 1 for upper letter of 0 for any else
+ */
+
+int _isupper(int x)
+
+{
+
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
