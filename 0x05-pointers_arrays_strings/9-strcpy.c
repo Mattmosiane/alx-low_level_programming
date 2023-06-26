eab233d15ac77410b150b5e5892df47227b518d2
Mattmosiane <matt.mosiane@gmail.com>
2023-06-26 13:40:47 -0700

    Task 9 strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string
+ * @dest: Destination value
+ * @src: source value
+ * Return: the pointers to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+
+}
