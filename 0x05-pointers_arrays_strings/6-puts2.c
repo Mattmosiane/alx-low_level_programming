18d87dfb772428f9f2ef6d0b979965bf039d3e7e
Mattmosiane <matt.mosiane@gmail.com>
2023-06-26 13:08:27 -0700

    Half the lies they tell about me

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be treated
+ * Return: void
+ */
+
+void puts2(char *str)
+
+{
+	int i;
+	int j;
+
+	while (str[j] != '\n')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
