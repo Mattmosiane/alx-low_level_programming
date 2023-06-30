e871090cee6c5c28631e98f2fcd828d3c6b3f38f
Mattmosiane <matt.mosiane@gmail.com>
2023-06-30 12:57:22 -0700

    It is the addition of strangeness to beauty

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -42,8 +42,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
-		while (*(n2 + j) != '\0')
-			j++;
+		i++;
+	
+	while (*(n2 + j) != '\0')
+		j++;
 	i--;
 	j--;
 	if (j >= size_r || i >= size_r)
