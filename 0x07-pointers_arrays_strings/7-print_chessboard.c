ca310c66b1e0ae89a8aa4cbcde607e2007aa7653
Mattmosiane <matt.mosiane@gmail.com>
2023-07-03 13:19:39 -0700

    Chess is a menta tourture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _chessboard - Prints chessboard
+ * @a: array
+ * Return: Always 0 (success)
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
