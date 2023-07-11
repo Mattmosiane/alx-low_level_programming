f81eea5b6cdb911306926829bae456e4b488c549
Mattmosiane <matt.mosiane@gmail.com>
2023-07-11 10:56:41 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,18 +15,21 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
+
 	while (str[i])
 	{
 		i++;
 	}
 	s = malloc((sizeof(char) * i) + 1);
+
 	if (s == NULL)
 		return (NULL);
+
 	while (a < i)
 	{
 		s[a] = str[a];
 		a++;
 	}
-	s[a] = '\n';
+	s[a] = '\0';
 	return (s);
 }
