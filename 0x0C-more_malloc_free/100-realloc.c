4cb80216154a751c736fc4537aee7300dfc2cb1f
Mattmosiane <matt.mosiane@gmail.com>
2023-07-13 05:18:41 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -40,7 +40,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		nptr[i] = ((char *) ptr) [i];
+		nptr[i] = ((char *) ptr)[i];
 	}
 	free(ptr);
 	return (nptr);
